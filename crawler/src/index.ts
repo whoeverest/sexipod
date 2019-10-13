f889c662c1c500a559e50812c2456e89665e8c44
Darko Bozhinovski <dbozhinovski@gmail.com>
2019-10-13 23:51:24 +0200

    Finishes up all the points from #5, plus some package upgrades

--- crawler/src/index.ts
+++ crawler/src/index.ts
@@ -1,17 +1,20 @@
 import * as express from 'express';
-import * as request from 'request';
+import axios from 'axios';
+import * as cheerio from 'cheerio';
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(`${__dirname}/client`));
 
-app.post('/crawl', (req, res) => {
-  res.send(req.body);
+app.post('/crawl', async (req, res) => {
+  const page = await axios(req.body.url);
+  const $ = cheerio.load(page.data);
+  const links = $('a').length;
+  res.send(`The page ${req.body.url} is ${page.data.length / 1000} kilobytes big, and contains ${links} links.`);
 });
 
 app.get('/', express.static(`${__dirname}/client`));
-
 
 const port = 3000;
 console.log('Crawler is online:', `http://localhost:${port}`);
