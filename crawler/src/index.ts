21d40854d517cee553920dc0084156e3ebf6d7d6
Darko Bozhinovski <dbozhinovski@gmail.com>
2019-10-13 23:14:06 +0200

    Includes static files in build; Adds shx for copying; Updates @types/node

--- crawler/src/index.ts
+++ crawler/src/index.ts
@@ -1,8 +1,14 @@
 import * as express from 'express';
+import * as request from 'request';
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(`${__dirname}/client`));
+
+app.post('/crawl', (req, res) => {
+  res.send(req.body);
+});
 
 app.get('/', express.static(`${__dirname}/client`));
 
