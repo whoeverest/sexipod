f4194665a8422369614a73bfde7b87d7b62c443c
Darko Bozhinovski <dbozhinovski@gmail.com>
2019-10-15 16:10:40 +0200

    Makes the url check a middleware; Adds extension checkup to avoid common media types;

--- crawler/src/index.ts
+++ crawler/src/index.ts
@@ -7,23 +7,31 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(`${__dirname}/client`));
 
-const checkURL = (url: string) => {
-  const parsed = new URL(url);
+const checkURL = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  const parsed = new URL(req.body.url);
   const hostnameFragments = parsed.hostname.split('.');
-  if (hostnameFragments[hostnameFragments.length -1 ] === 'mk') return true;
-  return false;
-}
 
-app.post('/crawl', async (req, res) => {
+  // Just the common ones to start with.
+  const ignoredMIME = ['css', 'js', 'jpg', 'jpeg', 'png', 'bmp', 'avi', 'mp4',' mp3', 'wav', 'webm', 'webp'];
+  const pathnameFragments = parsed.pathname.split('.');
+
+  if (hostnameFragments[hostnameFragments.length -1 ] !== 'mk') {
+    return res.status(400).send(`Non-mk domain received: ${req.body.url}.`);
+  };
+
+  if (ignoredMIME.includes(pathnameFragments[pathnameFragments.length - 1])) {
+    return res.status(400).send(`Non-crawlable extension detected: ${pathnameFragments[pathnameFragments.length - 1]}`);
+  }
+
+  next();
+};
+
+app.post('/crawl', checkURL, async (req, res) => {
   try {
-    if (checkURL(req.body.url)) {
-      const page = await axios(req.body.url);
-      const $ = cheerio.load(page.data);
-      const links = $('a').length;
-      res.send(`The page ${req.body.url} is ${page.data.length / 1000} kilobytes big, and contains ${links} links.`);
-    } else {
-      res.status(400).send('Non-mk domain received.');
-    }
+    const page = await axios(req.body.url);
+    const $ = cheerio.load(page.data);
+    const links = $('a').length;
+    res.send(`The page ${req.body.url} is ${page.data.length / 1000} kilobytes big, and contains ${links} links.`);
   } catch(err) {
     res.status(400).send(`Request for <u>${req.body.url}</u> failed. Reason: <pre>${err.message}</pre>`);
   }
